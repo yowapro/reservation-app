0cdd0b200609b6a9c6ceb559a9b15df177562c0e
yowa <a.desirable.relation@gmail.com>
2022-03-18 17:00:00 +0900

    プロダクトリスティング

---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+  products:any = [1,2,3,4];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
